41477e730a384f07691a326949800b4879a00f13
Ângelo Tadeucci <15664821+AngeloTadeucci@users.noreply.github.com>
2025-09-29 23:25:40 -0300

    Add BanWordParser and related unit tests (#55)
    Introduces BanWordParser for parsing ban words and UGC ban words from XML files. Adds BanWordParserTest to verify correct parsing and updates project version to 2.3.4.

---
+++ Maple2.File.Parser/BanWordParser.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using System.Xml;
+using System.Xml.Serialization;
+using Maple2.File.IO;
+using Maple2.File.IO.Crypto.Common;
+using Maple2.File.Parser.Xml.String;
+
+namespace Maple2.File.Parser;
+
+public class BanWordParser {
+    private readonly M2dReader xmlReader;
+    private readonly XmlSerializer nameSerializer;
+
+    public BanWordParser(M2dReader xmlReader) {
+        this.xmlReader = xmlReader;
+        nameSerializer = new XmlSerializer(typeof(StringMapping));
+    }
+
+    public IEnumerable<(int Id, string Name)> ParseBanWords() {
+        int i = 0;
+        foreach (PackFileEntry entry in xmlReader.Files.Where(entry => entry.Name.Contains("banword") && !entry.Name.Contains("ugc"))) {
+            XmlReader reader = xmlReader.GetXmlReader(entry);
+            var mapping = nameSerializer.Deserialize(reader) as StringMapping;
+
+            Debug.Assert(mapping != null);
+
+            Dictionary<int, string> banWords = mapping.key.ToDictionary(_ => i++, key => key.name);
+            foreach (var banWord in banWords) {
+                if (string.IsNullOrEmpty(banWord.Value)) {
+                    continue;
+                }
+                yield return (banWord.Key, banWord.Value);
+            }
+        }
+    }
+
+    public IEnumerable<(int Id, string Name)> ParseUgcBanWords() {
+        int i = 0;
+        foreach (PackFileEntry entry in xmlReader.Files.Where(entry => entry.Name.Contains("ugcbanword"))) {
+            XmlReader reader = xmlReader.GetXmlReader(entry);
+            var mapping = nameSerializer.Deserialize(reader) as StringMapping;
+
+            Debug.Assert(mapping != null);
+
+            Dictionary<int, string> banWords = mapping.key.ToDictionary(_ => i++, key => key.name);
+            foreach (var banWord in banWords) {
+                if (string.IsNullOrEmpty(banWord.Value)) {
+                    continue;
+                }
+                yield return (banWord.Key, banWord.Value);
+            }
+        }
+    }
+}
---
+++ Maple2.File.Tests/BanWordParserTest.cs
@@ -0,0 +1,39 @@
+﻿using Maple2.File.Parser;
+using Maple2.File.Parser.Enum;
+using Maple2.File.Parser.Tools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Maple2.File.Tests;
+
+[TestClass]
+public class BanWordParserTest {
+    [TestMethod]
+    public void TestBanWordParser() {
+        var locale = Locale.NA;
+        Filter.Load(TestUtils.XmlReader, locale.ToString(), "Live");
+        var parser = new BanWordParser(TestUtils.XmlReader);
+
+        int count = 0;
+        foreach ((int id, string name) in parser.ParseBanWords()) {
+            Assert.IsTrue(id >= 0);
+            Assert.IsFalse(string.IsNullOrEmpty(name));
+            count++;
+        }
+        Assert.AreEqual(6179, count);
+    }
+
+    [TestMethod]
+    public void TestUgcBanWordParser() {
+        var locale = Locale.NA;
+        Filter.Load(TestUtils.XmlReader, locale.ToString(), "Live");
+        var parser = new BanWordParser(TestUtils.XmlReader);
+
+        int count = 0;
+        foreach ((int id, string name) in parser.ParseUgcBanWords()) {
+            Assert.IsTrue(id >= 0);
+            Assert.IsFalse(string.IsNullOrEmpty(name));
+            count++;
+        }
+        Assert.AreEqual(1208, count);
+    }
+}
