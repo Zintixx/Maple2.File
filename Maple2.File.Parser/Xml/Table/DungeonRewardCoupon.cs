723f236db4f44bbca69c7a523892c80910bfedbc
Zin <62830952+Zintixx@users.noreply.github.com>
2026-05-12 18:58:43 -0700

    Parse Dungeon Reward Coupon

---
+++ Maple2.File.Parser/Xml/Table/DungeonRewardCoupon.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Serialization;
+
+namespace Maple2.File.Parser.Xml.Table;
+
+// ./data/xml/table/na/dungeonrewardcoupon.xml
+[XmlRoot("ms2")]
+public class DungeonRewardCouponRoot {
+    [XmlElement] public List<DungeonRewardCoupon> dungeonRewardCoupon;
+}
+
+public class DungeonRewardCoupon {
+    [XmlAttribute] public int id;
+    [XmlAttribute] public string ticketTag;
+    [XmlAttribute] public int baseItemID;
+    [XmlAttribute] public int maxExtraCount;
+    [XmlElement] public List<DungeonRewardBaseRate> baseRate;
+}
+
+public class DungeonRewardBaseRate {
+    [XmlAttribute] public int v;
+}
