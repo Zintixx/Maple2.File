026ee32e759670bebf55274e418bf8dddf404f2c
Zin <62830952+Zintixx@users.noreply.github.com>
2026-05-25 11:39:56 -0700

    Character Traits (#71)

---
+++ Maple2.File.Parser/Xml/Table/CharacterAbility.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Serialization;
+using M2dXmlGenerator;
+
+namespace Maple2.File.Parser.Xml.Table;
+
+// ./data/xml/table/characterability.xml
+[XmlRoot("ms2")]
+public class CharacterAbilityRoot {
+    [XmlElement] public List<CharacterAbility> ability;
+}
+
+public class CharacterAbility {
+    [XmlAttribute] public int id;
+    [XmlAttribute] public int categoryID;
+    [XmlAttribute] public int requireLevel;
+    [XmlAttribute] public int additionalEffectID;
+    [XmlAttribute] public int additionalEffectLevel;
+}
